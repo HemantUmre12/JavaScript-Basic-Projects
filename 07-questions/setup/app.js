16fc76cbe00df276d30ea2a06da7ca93cbff0ab6
Hemant <hemantumre12@gmail.com>
2022-01-06 18:42:54 +0530

    Done with the question.

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,33 @@
-//using selectors inside the element
-// traversing the dom
+const questionBtn = [...document.querySelectorAll(".question-btn")];
+const questions = [...document.querySelectorAll(".question")];
+
+// TODO using selectors inside the element
+
+questions.forEach( (question) => {
+    const btn = question.querySelector(".question-btn");
+    btn.addEventListener("click", () => {
+        questions.forEach(( item ) => {
+            if(item !== question) {
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle("show-text");
+    });
+});
+
+// TODO traversing the dom
+
+
+// questionBtn.forEach((btn) => {
+//     btn.addEventListener("click", (e) => {
+//         const parentQuestion = e.currentTarget.parentElement.parentElement;
+//         // removing class from other questions
+//         questions.forEach( (question) => {
+//             if(question !== parentQuestion) {
+//                 question.classList.remove("show-text");
+//             }
+//         })
+//         // console.log(parentQuestion);
+//         parentQuestion.classList.toggle("show-text");
+//     })
+// });
