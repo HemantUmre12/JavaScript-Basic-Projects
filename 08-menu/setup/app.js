a8b986945c96a518539314d22eb2be151498112a
Hemant <hemantumre12@gmail.com>
2022-01-07 22:51:52 +0530

    Done with the menu Project.

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -71,4 +71,72 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
+
+const sectionCenter = document.querySelector(".section-center");
+const categoryBtnsContainer = document.querySelector(".btn-container");
+
+// ! load items
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("Loaded");
+  displayMenuItems(menu);
+  displayFilterBtns();
+});
+
+// ! Displaying filter buttons dynamically by checking the category first
+
+function displayFilterBtns() {
+  const categories = menu.reduce(( values, item ) => {
+    if(!values.includes(item.category)) {
+      values.push(item.category);
+    }
+    return values;
+  },
+  ["all"]);
+  const categoryBtns = categories.map(( category ) => {
+    return `<button class="filter-btn" type = "button" data-category = "${category}">${category}</button>`;
+  })
+  .join("");
+  categoryBtnsContainer.innerHTML = categoryBtns;
+  const filterBtns = [...categoryBtnsContainer.querySelectorAll(".filter-btn")];
+  // ! filter items
+  filterBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      const category = e.currentTarget.dataset.category;
+      let filterCategory = menu.filter(( item ) => {
+        if(category === "all" || item.category === category) return item;
+      });
+      displayMenuItems( filterCategory );
+    });
+  });
+}
+
+
+// ! Display selected items
+function displayMenuItems( menuItems ) {
+  let displayMenu = menuItems.map(( item ) => {
+    // console.log(item);
+    return `<article class="menu-item">
+    <img src="${item.img}" alt="${item.title}" class="photo">
+    <div class="item-info">
+      <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">
+        ${item.desc}
+      </p>
+    </div>
+  </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  sectionCenter.innerHTML = displayMenu;
+} 
